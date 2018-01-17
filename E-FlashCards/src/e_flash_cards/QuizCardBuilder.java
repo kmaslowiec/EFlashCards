61a8d453fcaed1770cb2c35803e03097bec0564e
Kondzik <kmaslowiec@gmail.com>
2018-01-17 10:48:42 +0100

    version 0.2.5
    *the first versions of logic were added to newItem and nextButton

--- E-FlashCards/src/e_flash_cards/QuizCardBuilder.java
+++ E-FlashCards/src/e_flash_cards/QuizCardBuilder.java
@@ -3,6 +3,7 @@ package e_flash_cards;
 import java.awt.EventQueue;
 import java.awt.Font;
 
+import javax.swing.Action;
 import javax.swing.BorderFactory;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
@@ -18,6 +19,8 @@ import javax.swing.JToolBar;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class QuizCardBuilder extends JFrame {
 
@@ -29,6 +32,7 @@ public class QuizCardBuilder extends JFrame {
 	private JMenuBar menuBar;
 	private JMenu menuBarFile;
 	private JMenuItem menuItemNew;
+	private JButton nextButton;
 
 	/**
 	 * Launch the application.
@@ -47,40 +51,15 @@ public class QuizCardBuilder extends JFrame {
 	}
 
 	/**
-	 * Create the frame.
+	 * Create the frame. Constructor
 	 */
 	public QuizCardBuilder() {
 		gui();
 	}
 
-	public void questionArea() {
-		questionLabel = new JLabel("Question:");
-		questionLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
-
-		questionText = new JTextArea();
-		questionText.setBorder(BorderFactory.createLoweredBevelBorder());
-
-	}
-
-	public void answerArea() {
-		answerLabel = new JLabel("Answer:");
-		answerLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
-
-		answerText = new JTextArea();
-		answerText.setBorder(BorderFactory.createLoweredBevelBorder());
-
-	}
-
-	public void menuBar() {
-		menuBar = new JMenuBar();
-		setJMenuBar(menuBar);
-
-		menuBarFile = new JMenu("File");
-		menuBar.add(menuBarFile);
-
-		menuItemNew = new JMenuItem("New");
-		menuBarFile.add(menuItemNew);
-	}
+	/**
+	 * implements the whole GUI
+	 */
 
 	public void gui() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -93,9 +72,8 @@ public class QuizCardBuilder extends JFrame {
 		menuBar();
 		questionArea();
 		answerArea();
-
-		JButton btnNewButton = new JButton("NEXT CARD");
-		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 11));
+		buttonNext();
+		
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING).addGroup(gl_contentPane
 				.createSequentialGroup()
@@ -108,14 +86,106 @@ public class QuizCardBuilder extends JFrame {
 						.addGroup(gl_contentPane.createSequentialGroup().addGap(279).addComponent(answerLabel)))
 				.addContainerGap())
 				.addGroup(gl_contentPane.createSequentialGroup().addContainerGap(271, Short.MAX_VALUE)
-						.addComponent(btnNewButton).addGap(260)));
+						.addComponent(nextButton).addGap(260)));
 		gl_contentPane.setVerticalGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup().addGap(5).addComponent(questionLabel)
 						.addPreferredGap(ComponentPlacement.RELATED)
 						.addComponent(questionText, GroupLayout.PREFERRED_SIZE, 137, GroupLayout.PREFERRED_SIZE)
 						.addGap(18).addComponent(answerLabel).addPreferredGap(ComponentPlacement.UNRELATED)
 						.addComponent(answerText, GroupLayout.PREFERRED_SIZE, 310, GroupLayout.PREFERRED_SIZE)
-						.addGap(18).addComponent(btnNewButton).addContainerGap(40, Short.MAX_VALUE)));
+						.addGap(18).addComponent(nextButton).addContainerGap(40, Short.MAX_VALUE)));
 		contentPane.setLayout(gl_contentPane);
 	}
+
+	/**
+	 * Creates the question text field and label
+	 */
+	public void questionArea() {
+		questionLabel = new JLabel("Question:");
+		questionLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
+
+		questionText = new JTextArea();
+		questionText.setBorder(BorderFactory.createLoweredBevelBorder());
+
+	}
+
+	/**
+	 * Creates the answer text field and label
+	 */
+	public void answerArea() {
+		answerLabel = new JLabel("Answer:");
+		answerLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
+
+		answerText = new JTextArea();
+		answerText.setBorder(BorderFactory.createLoweredBevelBorder());
+
+	}
+	
+	/**
+	 * Creates button to "switch" to the next card
+	 */
+	
+	public void buttonNext() {
+		nextButton = new JButton("NEXT CARD");
+		nextButton.addActionListener(new ButtonListener());
+		nextButton.setFont(new Font("Tahoma", Font.BOLD, 11));
+	}
+	
+	/**
+	 * Creates Menu Bar with the items
+	 * 
+	 */
+
+	public void menuBar() {
+		menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+
+		menuBarFile = new JMenu("File");
+		menuBar.add(menuBarFile);
+
+		menuItemNew = new JMenuItem("New");
+		menuItemNew.addActionListener(new NewListener());
+		menuBarFile.add(menuItemNew);
+	}
+	
+	// Logic:
+	
+	public void printText() {
+		
+		String text = questionText.getText();
+		System.out.println(text);
+		
+	}
+
+	// Action listeners:
+
+	/**
+	 * 
+	 * Action listener for new in Bar Menu
+	 *
+	 */
+
+	public class NewListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+
+			questionText.setText(null);
+			answerText.setText(null);
+			questionText.requestFocus();
+
+		}
+
+	}
+	
+	public class ButtonListener implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			printText();
+			
+		}
+		
+	}
+	
 }
